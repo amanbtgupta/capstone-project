072f4766a4628e549094e9d04982d9b3ea73be9e
amanbtgupta <90843548+amanbtgupta@users.noreply.github.com>
2025-06-14 14:14:08 +0530

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run your setup tasks
+bash setup.sh
+
+# Navigate to the folder where app.py lives
+cd src/ui
+
+# Run Streamlit app on Azure-required port 8000
+streamlit run app.py --server.port=8000 --server.enableCORS=false
